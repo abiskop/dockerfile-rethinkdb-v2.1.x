43603676fa040eca075f89a231045339f548fa5f
Alexander Biskop <abiskop@gmail.com>
2015-09-07 14:28:01 +0200

    Added Dockerfile & install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+RETHINKDB_VERSION=2.1.2
+
+source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+wget -qO- http://download.rethinkdb.com/apt/pubkey.gpg | apt-key add -
+apt-get update -y
+apt-get install -y rethinkdb=$RETHINKDB_VERSION
